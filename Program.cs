66db99a5a5d8b326f4c9f6e80e212a1555f7b9d8
gabrielSoares522 <gabrielcsoares52@gmail.com>
2022-02-13 16:16:36 -0300

    criando classe calculadora

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CalculadoraExceptionTest;
+
+class Program{
+    public double Somar(double x,double y){
+        
+        
+        return x+y;
+    }
+
+    public double Subtrair(double x,double y){
+        return x-y;
+    }
+
+    public double Dividir(double x,double y){
+        return x/y;
+    }
+
+    public double Multiplicar(double x,double y){
+        return x*y;
+    }
+}
